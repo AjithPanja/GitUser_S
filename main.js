4bb6a14b7f918fb7b2140ca72a7097b069a66758
AjithPanja <ajithkumarpanja@gmail.com>
2018-10-30 03:34:17 +0530

    deleted unnessesary codes

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const client_id = "Iv1.74f61ef73d6054b2";
-const client_secret = "01ac1f4946e1726281107327a0ed2b874d02ca5d";
-
 const user = document.querySelector("#user_n");
 const butt = document.querySelector("#submit");
 const get_user = document.querySelector(".username");
@@ -8,7 +5,7 @@ const repo = document.querySelector(".Repository");
 const url = document.querySelector(".link");
 
 const fetch_user = async(userr) => {
-  const api_call = await fetch(`https://api.github.com/users/${userr}?client_id=${client_id}&client_secret=${client_secret}`);
+  const api_call = await fetch(`https://api.github.com/search/users?q=${userr}`);
   const data = await api_call.json();
   return {data};
 };
