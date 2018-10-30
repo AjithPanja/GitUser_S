a0e2f69e39d1297b6cee3695cce01caaa4ed471f
AjithPanja <ajithkumarpanja@gmail.com>
2018-10-30 16:02:36 +0530

    phase-1 working module

--- main.js
+++ main.js
@@ -13,13 +13,35 @@ const fetch_user = async(userr) => {
 const show = () =>
 {
   fetch_user(user.value).then((res)=>{
-    get_user.innerHTML = `Name : ${res.data.name}`;
-    repo.innerHTML = `Repositories : ${res.data.public_repos}`;
-    url.innerHTML = `Link : ${res.data.html_url}`;
+    // get_user.innerHTML = `Name : ${res.data.items[2].name}`;
+    // repo.innerHTML = `Repositories : ${res.data.items[2].public_repos}`;
+    // url.innerHTML = `Link : ${res.data.items[2].html_url}`;
+    var i;
+    for(i=1;i<=3;i++)
+    {
+      var id = 'no'+i;
+      var img = 'img'+i;
+      console.log(id);
+      console.log(img);
+      document.getElementById(img).innerHTML=`<img style = "width:100%" src = ${res.data.items[i-1].avatar_url}></img><div class = "containerr"><p> ${res.data.items[i-1].login}</p></div>`;
+      document.getElementById(img).href=`${res.data.items[i-1].html_url}`;
+    }
+    document.addEventListener('DOMContentLoaded', function() {
+       var elems = document.querySelectorAll('.carousel');
+       var instances = M.Carousel.init(elems, false);
+     });
     console.log(res);
+    caro();
   })
 };
 
 butt.addEventListener("click" , () => {
   show();
 });
+
+var caro = () =>
+{document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.carousel');
+   var instances = M.Carousel.init(elems, duration = '100');
+ });
+};
